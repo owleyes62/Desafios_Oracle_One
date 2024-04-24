c2f84ad95f9053aa5e27d45021619051ce9ab7bd
Iwerson <guilhermepdo2012@gmail.com>
2024-04-24 09:44:32 -0300

    implementing conversion and user menu

--- conversor_de_moedas/src/main/java/meuapp/CurrencyRates.java
+++ conversor_de_moedas/src/main/java/meuapp/CurrencyRates.java
@@ -9,6 +9,7 @@ public class CurrencyRates {
         this.rates = jsonObject.getAsJsonObject("rates");
     }
 
+    // Métodos para obter as taxas de câmbio de cada moeda
     public double getARSRate() {
         return rates.get("ARS").getAsDouble();
     }
@@ -31,5 +32,31 @@ public class CurrencyRates {
 
     public double getUSDRate() {
         return rates.get("USD").getAsDouble();
+    }
+
+    // Métodos para realizar as conversões entre moedas
+    public double convertToARS(double amount) {
+        return amount * getARSRate();
+    }
+
+    public double convertToBOB(double amount) {
+        return amount * getBOBRate();
+    }
+
+    public double convertToBRL(double amount) {
+        return amount * getBRLRate();
+    }
+
+    public double convertToCLP(double amount) {
+        return amount * getCLPRate();
+    }
+
+    public double convertToCOP(double amount) {
+        return amount * getCOPRate();
+    }
+
+    // Método para converter de uma moeda para outra qualquer
+    public double convertToCurrency(double amount, double targetRate) {
+        return amount * targetRate;
     }
 }
--- conversor_de_moedas/src/main/java/meuapp/ExchangeRateApiExample.java
+++ conversor_de_moedas/src/main/java/meuapp/ExchangeRateApiExample.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -12,6 +13,8 @@ import com.google.gson.JsonObject;
 public class ExchangeRateApiExample {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://api.exchangerate-api.com/v4/latest/USD"))
@@ -27,16 +30,55 @@ public class ExchangeRateApiExample {
             // Criando uma instância de CurrencyRates
             CurrencyRates currencyRates = new CurrencyRates(jsonObject);
 
-            // Acessando os valores das moedas e imprimindo
-            System.out.println("ARS: " + currencyRates.getARSRate());
-            System.out.println("BOB: " + currencyRates.getBOBRate());
-            System.out.println("BRL: " + currencyRates.getBRLRate());
-            System.out.println("CLP: " + currencyRates.getCLPRate());
-            System.out.println("COP: " + currencyRates.getCOPRate());
-            System.out.println("USD: " + currencyRates.getUSDRate());
+            while (true) {
+                System.out.println("\nEscolha uma opção:");
+                System.out.println("1 - Converter USD para ARS");
+                System.out.println("2 - Converter USD para BOB");
+                System.out.println("3 - Converter USD para BRL");
+                System.out.println("4 - Converter USD para CLP");
+                System.out.println("5 - Converter USD para COP");
+                System.out.println("0 - Sair");
+
+                int opcao = scanner.nextInt();
+
+                if (opcao == 0) {
+                    break;
+                }
+
+                System.out.println("Digite o valor em USD:");
+                double amountUSD = scanner.nextDouble();
+
+                switch (opcao) {
+                    case 1:
+                        double amountARS = currencyRates.convertToARS(amountUSD);
+                        System.out.println("ARS: " + amountARS);
+                        break;
+                    case 2:
+                        double amountBOB = currencyRates.convertToBOB(amountUSD);
+                        System.out.println("BOB: " + amountBOB);
+                        break;
+                    case 3:
+                        double amountBRL = currencyRates.convertToBRL(amountUSD);
+                        System.out.println("BRL: " + amountBRL);
+                        break;
+                    case 4:
+                        double amountCLP = currencyRates.convertToCLP(amountUSD);
+                        System.out.println("CLP: " + amountCLP);
+                        break;
+                    case 5:
+                        double amountCOP = currencyRates.convertToCOP(amountUSD);
+                        System.out.println("COP: " + amountCOP);
+                        break;
+                    default:
+                        System.out.println("Opção inválida!");
+                        break;
+                }
+            }
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            scanner.close();
         }
     }
 }
